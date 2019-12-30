4085f075c27085af534c3799cd2c2d53e2ac5f7d
Josemar Luedke <josemarluedke@gmail.com>
2019-12-30 12:51:41 -0800

    Add code-snippet & code syntax highlight

--- docs/ember-cli-build.js
+++ docs/ember-cli-build.js
@@ -5,6 +5,8 @@ const path = require('path');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
+    snippetPaths: ['app/snippets'],
+
     'ember-power-select': {
       theme: false
     },
@@ -21,19 +23,6 @@ module.exports = function(defaults) {
       }
     }
   });
-
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
 
   return app.toTree();
 };
