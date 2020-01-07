545287b5eae7643dcb2ded751fb0ce354d047caa
Josemar Luedke <josemarluedke@gmail.com>
2020-01-06 22:31:46 -0800

    [Docs] Add css to code-block

--- docs/app/components/code-block.js
+++ docs/app/components/code-block.js
@@ -5,6 +5,7 @@ import hljs from 'highlight.js/lib/highlight';
 import javascript from 'highlight.js/lib/languages/javascript';
 import shell from 'highlight.js/lib/languages/shell';
 import htmlbars from 'highlight.js/lib/languages/htmlbars';
+import css from 'highlight.js/lib/languages/css';
 
 hljs.registerLanguage('javascript', javascript);
 hljs.registerLanguage('js', javascript);
@@ -12,6 +13,7 @@ hljs.registerLanguage('handlebars', htmlbars);
 hljs.registerLanguage('hbs', htmlbars);
 hljs.registerLanguage('shell', shell);
 hljs.registerLanguage('sh', shell);
+hljs.registerLanguage('css', css);
 
 export default class CodeBlockComponent extends Component {
   @action setupHighlight(element) {
