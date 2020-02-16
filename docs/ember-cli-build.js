eb77ff703eab9aa3469c9cc18d8ea9033676fa9e
Josemar Luedke <josemarluedke@gmail.com>
2020-02-15 19:03:48 -0800

    Add PurgeCSS to docs

--- docs/ember-cli-build.js
+++ docs/ember-cli-build.js
@@ -3,6 +3,14 @@
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 const path = require('path');
 
+const purgecssOptions = {
+  content: ['./app/index.html', './app/**/*.hbs', './node_modules/**/*.hbs'],
+  defaultExtractor: content => {
+    return content.match(/[A-Za-z0-9-_:/]+/g) || [];
+  },
+  whitelistPatterns: [/mode-dark/, /hljs/]
+};
+
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     snippetPaths: ['app/snippets'],
@@ -18,7 +26,8 @@ module.exports = function(defaults) {
           require('tailwindcss')(
             path.join('app', 'styles', 'tailwind.config.js')
           ),
-          require('autoprefixer')
+          require('autoprefixer'),
+          require('@fullhuman/postcss-purgecss')(purgecssOptions)
         ]
       }
     }
