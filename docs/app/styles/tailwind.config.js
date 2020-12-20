9ccb4ec20ce735484802258136548ace87200e7e
Josemar Luedke <josemarluedke@gmail.com>
2020-12-19 21:02:44 -0800

    Adjust a few colors in docs

--- docs/app/styles/tailwind.config.js
+++ docs/app/styles/tailwind.config.js
@@ -27,7 +27,7 @@ module.exports = {
           '--power-select-placeholder-text-color': theme('colors.gray.500'),
           '--power-select-background-color': theme('colors.white'),
           '--power-select-dropdown-background-color': theme('colors.white'),
-          '--power-select-border-color': theme('colors.gray.500'),
+          '--power-select-border-color': theme('colors.gray.400'),
           '--power-select-selected-background-color': theme('colors.gray.200'),
           '--power-select-multiple-option-background-color': theme(
             'colors.gray.600'
@@ -35,15 +35,15 @@ module.exports = {
         },
         ['.dark']: {
           '--power-select-text-color': theme('colors.gray.300'),
-          '--power-select-disabled-text-color': theme('colors.gray.700'),
-          '--power-select-disabled-border-color': theme('colors.gray.800'),
-          '--power-select-placeholder-text-color': theme('colors.gray.700'),
+          '--power-select-disabled-text-color': theme('colors.gray.600'),
+          '--power-select-disabled-border-color': theme('colors.gray.700'),
+          '--power-select-placeholder-text-color': theme('colors.gray.600'),
           '--power-select-background-color': theme('colors.gray.900'),
           '--power-select-dropdown-background-color': theme('colors.gray.900'),
-          '--power-select-border-color': theme('colors.gray.600'),
-          '--power-select-selected-background-color': theme('colors.gray.700'),
+          '--power-select-border-color': theme('colors.gray.500'),
+          '--power-select-selected-background-color': theme('colors.gray.600'),
           '--power-select-multiple-option-background-color': theme(
-            'colors.gray.700'
+            'colors.gray.600'
           )
         }
       });
