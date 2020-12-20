a5757b879324e1ca0232f75c352db00e3d1cb6c1
Josemar Luedke <josemarluedke@gmail.com>
2020-12-19 20:58:13 -0800

    Fix code highlight

--- docs/app/components/code-block.js
+++ docs/app/components/code-block.js
@@ -1,22 +1,22 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
-import hljs from 'highlight.js/lib/highlight';
+import hljs from 'highlight.js';
 
 import javascript from 'highlight.js/lib/languages/javascript';
 import shell from 'highlight.js/lib/languages/shell';
 import htmlbars from 'highlight.js/lib/languages/htmlbars';
 import css from 'highlight.js/lib/languages/css';
 
-// hljs.registerLanguage('javascript', javascript);
-// hljs.registerLanguage('js', javascript);
-// hljs.registerLanguage('handlebars', htmlbars);
-// hljs.registerLanguage('hbs', htmlbars);
-// hljs.registerLanguage('shell', shell);
-// hljs.registerLanguage('sh', shell);
-// hljs.registerLanguage('css', css);
+hljs.registerLanguage('javascript', javascript);
+hljs.registerLanguage('js', javascript);
+hljs.registerLanguage('handlebars', htmlbars);
+hljs.registerLanguage('hbs', htmlbars);
+hljs.registerLanguage('shell', shell);
+hljs.registerLanguage('sh', shell);
+hljs.registerLanguage('css', css);
 
 export default class CodeBlockComponent extends Component {
   @action setupHighlight(element) {
-    // hljs.highlightBlock(element);
+    hljs.highlightBlock(element);
   }
 }
